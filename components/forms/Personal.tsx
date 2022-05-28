07e3eac1a087a6ba417fabb229541e1a0ba96d18
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2022-05-28 20:03:09 +0100

    Abstract reusable logic and components into a different file

--- components/forms/Personal.tsx
+++ components/forms/Personal.tsx
@@ -1,38 +1,13 @@
-import { Typography } from "@mui/material";
-import TextField from "@mui/material/TextField"
-import { useFormContext } from "../../context/FormContext";
-import styles from '../../styles/Personal.module.css';
+import styles from '../../styles/Forms.module.css';
+import Input from "../Input";
 
-const inputs = ['First Name', 'Middle Name', 'Last Name', 'Referral Code (Optional)']
-
-const generateInputName = (input: string) => {
-  const split = input.split(' ');
-  const name = split.map((item, index) => {
-    if (index === 0) return item.toLowerCase();
-    else if (index === 1) return item;
-    return;
-  });
-
-  return name.join('');
-}
+const inputs = ['First Name', 'Middle Name', 'Last Name', 'Referral Code (Optional)'];
 
 export default function Personal() {
-  const [formData, updateFormData] = useFormContext();
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    updateFormData({[e.target.name]: e.target.value})
-  }
-
   return (
     <div className={styles.form_container}>
       {inputs.map((input, index, arr) => (
-        <>
-          <Typography component='p' className={styles.input_label}>{input}</Typography>
-          <TextField
-            key={input} className={styles.input} name={generateInputName(input)}
-            type='text' placeholder={input} variant="outlined" size='small' required={index !== arr.length - 1}
-            onChange={handleChange}  
-            />
-        </>
+        <Input key={input} input={input} type={input} required={index !== arr.length - 1} />
       ))}
     </div>
   )
